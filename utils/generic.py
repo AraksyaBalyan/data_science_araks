d3a7237255665aa1f36e6fb0e8417c734d177cde
araksbalyan50-spec <araksbalyan50@gmail.com>
2026-02-05 21:41:35 +0400

    added function array_average

--- utils/generic.py
+++ utils/generic.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import numpy as np
 
 def read_file_pandas(filename):
     csv_file_data = pd.read_csv(filename)
     return csv_file_data
+
+
+def array_average(arr):
+    return sum(arr) / len(arr)
+
 
