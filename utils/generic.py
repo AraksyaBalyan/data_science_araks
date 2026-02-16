9e5105818dd50fd4cb104cf4fd5d96ffb194185f
araksbalyan50-spec <araksbalyan50@gmail.com>
2026-02-16 23:58:47 +0400

    added 3 new functions

--- utils/generic.py
+++ utils/generic.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import numpy as np
 
-def read_file_pandas(filename):
-    csv_file_data = pd.read_csv(filename)
-    return csv_file_data
+
+def read_file_pandas(filename, columns):
+    try:
+        csv_file_data = pd.read_csv(filename)
+    except KeyError:
+        print("KeyError: Column->", list(csv_file_data.columns), f"Provided -> {columns}")
+        return None
+    except FileNotFoundError:
+        print("Message-> ")
 
 
 def array_average(arr):
     return np.average(arr)
 
+
 def min_max_array(arr):
-    minimum = arr.max()
-    maximum = arr.min()
+    minimum = arr.min()
+    maximum = arr.max()
     return minimum, maximum
 
 
@@ -20,7 +27,19 @@ def get_column_from_csv(file_path, column_name):
 
     return df[column_name]
 
-result = get_column_from_csv('E:\Downloads\ archive (2)\world_university_survey_dataset.csv', 'age')
-print(result)
-print(array_average(result))
-print(min_max_array(result))
+
+def print_column_names(df):
+    for col in df.columns:
+        return f"column names are - {col}"
+
+
+def print_basic_info(df):
+    return df.info()
+
+
+def analyze_missing_values(df):
+    missing_data = df.isnull().sum()
+    if missing_data.sum() == 0:
+        return "All cells are filled (there are no missing values)."
+    else:
+        return "missing_data[missing_data > 0]"
